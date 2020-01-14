4e7016581b41dbdad3758a4fc2962448d8b58647
Dilraj <dsomel21@gmail.com>
2020-01-13 21:16:16 -0500

    Mutation for register works

---
+++ server/src/UserResolver.ts
@@ -0,0 +1,43 @@
+import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import bcrypt from 'bcrypt';
+import { User } from './entity/User';
+
+// The code below does the same thing...
+// typeDefs: `
+// type Query {
+//   hello: String!,
+// }`,
+// resolvers: {
+//   Query: {
+//     hello: () => 'hello world',
+//   },
+// }
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return 'hi';
+  }
+
+  // POSTing
+  @Mutation(() => Boolean)
+  async register(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      await User.insert({
+        email: email,
+        password: hashedPassword,
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
+  }
+}
+
+// Mutation -
--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,18 +1,13 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 
-@Entity()
-export class User {
+@Entity('users')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column('text')
+  email: string;
 
-    @Column()
-    firstName: string;
-
-    @Column()
-    lastName: string;
-
-    @Column()
-    age: number;
-
+  @Column('text')
+  password: string;
 }
