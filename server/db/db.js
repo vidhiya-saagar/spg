29ae1da62dd3c1a89f08dfa92b494324e719504b
Dilraj Somel <dsomel21@gmail.com>
2020-08-17 12:29:08 -0400

    Got the "chapters" table working. MySQL migrations + insertions working!

---
+++ server/db/db.js
@@ -0,0 +1,12 @@
+// const knex = require('knex');
+// require('dotenv').config();
+
+// knex({
+// client: 'pg',
+// connection: {
+//   host: '127.0.0.1',
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DEVELOPMENT_DATABASE,
+// },
+// });
