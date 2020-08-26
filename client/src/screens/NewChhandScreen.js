af59fd67c8d4ccf43d6cf68c9238d8c6b4b66a50
Dilraj Somel <dsomel21@gmail.com>
2020-08-25 20:59:02 -0400

    NewChhandScreen is coming along

--- client/src/screens/NewChhandScreen.js
+++ client/src/screens/NewChhandScreen.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '../components/Grid';
+import { Formik, Field, Form } from 'formik';
+import { fetchGet } from '../helpers/fetchHelper';
+
+const TextField = (props) => (
+  <>
+    <h1>Hi {props.name}</h1>
+  </>
+);
 
 const NewChhandScreen = () => {
+  const [newChhandOrderNumber, setNewChhandOrderNumber] = useState(0);
+
+  useEffect(() => {
+    const getLastChhand = async () => {
+      try {
+        const res = await fetchGet('/chhands/last');
+        setNewChhandOrderNumber(res.chhand.order_number + 1);
+      } catch (error) {
+        console.log('⚠️ Error fetching the last Chhand.');
+      }
+    };
+    getLastChhand();
+  });
+
+  const initialValues = {
+    chhandType: '',
+    orderNumber: '',
+  };
+
   return (
     <>
       <Grid alignItems='center' justify='center'>
         <Grid column={true} sm={8} md={8} lg={8}>
           <p className='title'>New Chhand</p>
+        </Grid>
+      </Grid>
+
+      <Grid alignItems='center' justify='center'>
+        <Grid column={true} sm={8} md={8} lg={8}>
+          <TextField name={'Dilraj'} />
         </Grid>
       </Grid>
     </>
