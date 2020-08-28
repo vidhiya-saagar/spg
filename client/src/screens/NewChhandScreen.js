6aebe6f59ae84959cfc6bb1411956f1f673757f9
Dilraj Somel <dsomel21@gmail.com>
2020-08-28 19:46:05 -0400

    Minor CSS changes

--- client/src/screens/NewChhandScreen.js
+++ client/src/screens/NewChhandScreen.js
@@ -13,16 +13,19 @@ const NewChhandScreen = () => {
   const [newChhandOrderNumber, setNewChhandOrderNumber] = useState(0);
 
   useEffect(() => {
+    console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
     const getLastChhand = async () => {
       try {
         const res = await fetchGet('/chhands/last');
-        setNewChhandOrderNumber(res.chhand.order_number + 1);
+        debugger;
+        console.log(res);
+        setNewChhandOrderNumber(() => res.chhand.order_number + 1);
       } catch (error) {
         console.log('⚠️ Error fetching the last Chhand.');
       }
     };
     getLastChhand();
-  });
+  }, []);
 
   const initialValues = {
     chhandType: '',
@@ -33,7 +36,7 @@ const NewChhandScreen = () => {
     <>
       <Grid alignItems='center' justify='center'>
         <Grid column={true} sm={8} md={8} lg={8}>
-          <p className='title'>New Chhand</p>
+          <h1 className='title'>New Chhand</h1>
         </Grid>
       </Grid>
 
