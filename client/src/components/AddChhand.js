664a4996141b18e3bec421109bd33321b9bf83a2
Dilraj Somel <dsomel21@gmail.com>
2020-08-29 15:55:54 -0400

    Implementation of `chhand_types` select dropdown menu

--- client/src/components/AddChhand.js
+++ client/src/components/AddChhand.js
@@ -3,14 +3,30 @@ import Grid from '../components/Grid';
 import * as anvaad from 'anvaad-js';
 import { fetchGet } from '../helpers/fetchHelper';
 import { CodeBlock, a11yLight } from 'react-code-blocks';
+import Select from 'react-select';
+
+const selectStyles = {
+  control: (provided) => ({
+    ...provided,
+    borderRadius: 20,
+    fontFamily: 'Josefin Sans',
+    borderWidth: 1.4,
+    borderColor: '#e1e1e1',
+    letterSpacing: 1,
+    color: '#242424',
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: '0.75rem',
+    paddingRight: '0.75rem',
+  }),
+};
 
 const AddChhand = () => {
-  const [chhandTypeOptions, setChhandTypeOptions] = useState(null);
+  const [chhandTypeOptions, setChhandTypeOptions] = useState([]);
   const [lastChapter, setLastChapter] = useState(null);
 
   const [unicode, setUnicode] = useState('');
-  const [chhandType, setChhandType] = useState('');
-  const [english, setEnglish] = useState('');
+  const [selectedChhandType, setSelectedChhandType] = useState(null);
 
   const chapterCode = `{
     id: ${lastChapter?.id},
@@ -23,7 +39,14 @@ const AddChhand = () => {
     const fetchAllChhandTypes = async () => {
       const res = await fetchGet('/chhand-types');
       console.log('fetchAllChhandTypes', res);
-      setChhandTypeOptions(res.chhand_types);
+      setChhandTypeOptions(
+        res.chhand_types.map((chhandType) => {
+          return {
+            value: chhandType.id,
+            label: chhandType.chhand_name_english,
+          };
+        })
+      );
     };
 
     const fetchLastChapter = async () => {
@@ -36,73 +59,49 @@ const AddChhand = () => {
     };
     fetchAllChhandTypes();
     fetchLastChapter();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Grid alignItems='center' justify='center'>
       <Grid sm={12} md={8} lg={8} justify='center'>
         {/* Chapter */}
-
         <form className='spg-form'>
-          <label htmlFor='chapterNumber'>Chapter</label>
-          <CodeBlock
-            theme={a11yLight}
-            text={chapterCode}
-            language={'json'}
-            showLineNumbers={false}
-            wrapLines={false}
-            codeBlock
-            customStyle={{ borderRadius: 20, marginBottom: 15 }}
-          />
+          <div className='form-element'>
+            <label htmlFor='chapterNumber'>Chapter</label>
+            <CodeBlock
+              theme={a11yLight}
+              text={chapterCode}
+              language={'json'}
+              showLineNumbers={false}
+              wrapLines={false}
+              codeBlock
+              customStyle={{ borderRadius: 20, marginBottom: 15 }}
+            />
+          </div>
 
-          {/* Chapter */}
-          <label htmlFor='chhandOrderNumber'>Chapter</label>
-          <input
-            id='chhandOrderNumber'
-            name='chhandOrderNumber'
-            readOnly
-            type='text'
-            placeholder='ਸਿਰਖੰਡੀ ਛੰਦ'
-            value={lastChapter?.number}
-          />
+          {/* chhandOrderNumber  */}
+          <div className='form-element'>
+            <label htmlFor='chhandOrderNumber'>Chhand Order</label>
+            <input
+              id='chhandOrderNumber'
+              name='chhandOrderNumber'
+              readOnly
+              value={2}
+            />
+          </div>
 
-          {/* Unicode */}
-          <label htmlFor='unicode'>Gurmukhi Unicode</label>
-          <input
-            id='unicode'
-            name='unicode'
-            type='text'
-            placeholder='ਸਿਰਖੰਡੀ ਛੰਦ'
-            onChange={(e) => {
-              setUnicode(e.target.value);
-              setChhandType(anvaad.unicode(e.target.value, true));
-            }}
-            value={unicode}
-          />
+          {/* Selected Chhand  */}
+          <div className='form-element'>
+            <label htmlFor='chhandOrderNumber'>Chhand Type</label>
+            <Select
+              defaultValue={selectedChhandType}
+              onChange={setSelectedChhandType}
+              options={chhandTypeOptions}
+              styles={selectStyles}
+            />
+          </div>
 
-          {/* Gurmukhi Script */}
-          <label htmlFor='chhandType'>Gurmukhi Script</label>
-          <input
-            className='gurakhar'
-            id='chhandType'
-            name='chhandType'
-            type='text'
-            placeholder='isrKMfI CMd'
-            value={chhandType}
-          />
-
-          {/* English */}
-          <label htmlFor='english'>English</label>
-          <input
-            id='english'
-            name='english'
-            type='text'
-            placeholder='Sirkhandi Chhand'
-            onChange={(e) => {
-              setEnglish(e.target.value);
-            }}
-            value={english}
-          />
           <button type='submit' className={`mtop15 `}>
             Submit
           </button>
