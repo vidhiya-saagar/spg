47c87d451ca02fff38dab5b7ac70f10a4808f259
Dilraj Somel <dsomel21@gmail.com>
2020-09-11 22:04:04 -0400

    Ref #10 Creating the live preview of the pauri being added

---
+++ client/src/components/PauriPreview.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import TukStyles from '../stylesheets/TukStyles.module.css';
+import { Context as GranthContext } from '../context/GranthContext';
+import { getFormattedSignatureObj } from '../helpers/remap';
+const PauriPreview = (props) => {
+  const { state: granthState } = useContext(GranthContext);
+
+  const { pauri, nextPauriNum } = props;
+  return (
+    <div className={TukStyles.Bold}>
+      {pauri.map((tuk) => {
+        console.log(granthState);
+        return (
+          <div key={tuk.id}>
+            {/* 
+              <p>{tuk.content_unicode}</p>
+             */}
+            <p className='gurakhar'>{tuk.content_gs}</p>
+            {/*  
+            <p className={TukStyles.EnglishTranslit}>
+              {tuk.content_transliteration_english}
+            </p>             
+             */}
+          </div>
+        );
+      })}
+      <p>{getFormattedSignatureObj(nextPauriNum).signature_unicode}</p>
+    </div>
+  );
+};
+
+export default PauriPreview;
--- client/src/helpers/remap.js
+++ client/src/helpers/remap.js
@@ -41,5 +41,35 @@ export const formattedTukFormObj = (tukForm) => {
     thamki: 'thamkis',
     vishraam: 'vishraams',
   };
+
   return reMapAll(mappings, [], tukForm);
+};
+
+const numToGurmukhi = (num) => {
+  const mappings = {
+    0: '੦',
+    1: '੧',
+    2: '੨',
+    3: '੩',
+    4: '੪',
+    5: '੫',
+    6: '੬',
+    7: '੭',
+    8: '੮',
+    9: '੯',
+  };
+  return num
+    .toString()
+    .split('')
+    .map((c) => mappings[c])
+    .join('');
+};
+
+export const getFormattedSignatureObj = (num) => {
+  if (num == null || num === 0) return;
+  return {
+    signature_unicode: `॥${numToGurmukhi(num)}॥`,
+    signature_gs: `]${num}]`,
+    signature_english: `||${num}||`,
+  };
 };
