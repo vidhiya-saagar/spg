f8695b1352644089451ea0643418c3dba22312e7
Dilraj Somel <dsomel21@gmail.com>
2020-09-30 22:30:09 -0400

    Added a fetchDelete helper in fetchHelper file

--- client/src/helpers/fetchHelper.js
+++ client/src/helpers/fetchHelper.js
@@ -1,25 +1,30 @@
 import * as fetch from 'node-fetch';
 const baseUrl = 'http://localhost:1469/api/v1';
 
+export const fetchGet = async (url = '', data = {}) => {
+  console.log('GET', url);
+  const response = await fetch(baseUrl + url, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  return await response.json();
+};
+
 export const fetchPost = async (url = '', data = {}) => {
   console.log('POST', url);
   const response = await fetch(baseUrl + url, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   });
   return await response.json();
 };
 
-export const fetchGet = async (url = '', data = {}) => {
-  console.log('GET', url);
+export const fetchDelete = async (url = '') => {
+  console.log('DELETE', url);
   const response = await fetch(baseUrl + url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
   });
   return await response.json();
 };
